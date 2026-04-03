e2412e16704768778b8d4c5c805db5c0bb21a162
argentum3695 <argentum3695@gmail.com>
2026-04-03 21:55:22 +1100

    updated tags and styling: git push

--- projects/projects.js
+++ projects/projects.js
@@ -2,9 +2,11 @@ function filterProjects() {
 
     var elecCheckBox = document.getElementById("check-elec");
     var codeCheckBox = document.getElementById("check-code");
+    var inProgressCheckBox = document.getElementById("check-in-progress");
 
     const elecProjects = document.getElementsByClassName("elec-project");
     const codeProjects = document.getElementsByClassName("code-project");
+    const inProgressProjects = document.getElementsByClassName("project-in-progress");
 
     console.log(codeProjects);
 
@@ -27,6 +29,17 @@ function filterProjects() {
         // console.log("hiding code projects");
          for (let i = 0; i < codeProjects.length; i++) {
             codeProjects[i].style.display = "none";
+        }
+    }
+
+    if (inProgressCheckBox.checked) {
+        for (let i = 0; i < inProgressProjects.length; i++) {
+            inProgressProjects[i].style.display = "inline";
+        }
+    } else {
+        // console.log("hiding code projects");
+         for (let i = 0; i < inProgressProjects.length; i++) {
+            inProgressProjects[i].style.display = "none";
         }
     }
 
