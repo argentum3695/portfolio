a9ba9dd2492a035cc0b976fa80ec48944b7dd265
argentum3695 <argentum3695@gmail.com>
2026-04-05 19:50:53 +1000

    Added tile and tag for scribelator on projects page

--- projects/projects.js
+++ projects/projects.js
@@ -3,10 +3,12 @@ function filterProjects() {
     var elecCheckBox = document.getElementById("check-elec");
     var codeCheckBox = document.getElementById("check-code");
     var inProgressCheckBox = document.getElementById("check-in-progress");
+    var utilityCheckBox = document.getElementById("check-utility");
 
     const elecProjects = document.getElementsByClassName("elec-project");
     const codeProjects = document.getElementsByClassName("code-project");
     const inProgressProjects = document.getElementsByClassName("project-in-progress");
+    const utilityProjects = document.getElementsByClassName("utility-project");
 
     console.log(codeProjects);
 
@@ -26,7 +28,6 @@ function filterProjects() {
             codeProjects[i].style.display = "inline";
         }
     } else {
-        // console.log("hiding code projects");
          for (let i = 0; i < codeProjects.length; i++) {
             codeProjects[i].style.display = "none";
         }
@@ -37,9 +38,19 @@ function filterProjects() {
             inProgressProjects[i].style.display = "inline";
         }
     } else {
-        // console.log("hiding code projects");
          for (let i = 0; i < inProgressProjects.length; i++) {
             inProgressProjects[i].style.display = "none";
+        }
+    }
+
+    console.log(utilityCheckBox.checked);
+    if (utilityCheckBox.checked) {
+        for (let i = 0; i < utilityProjects.length; i++) {
+            utilityProjects[i].style.display = "inline";
+        }
+    } else {
+         for (let i = 0; i < utilityProjects.length; i++) {
+            utilityProjects[i].style.display = "none";
         }
     }
 
